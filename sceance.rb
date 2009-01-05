b59f413b10e7a135393c8ca568ae88679e5ecac9
Alexandre Girard <alx.girard@gmail.com>
2009-01-05 01:10:36 +0100

    first commit

---
+++ sceance.rb
@@ -0,0 +1,39 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+deputies = Hash.new
+
+def deputy_name(deputy_id)
+  fiche = "http://www.assemblee-nationale.fr/13/tribun/fiches_id/#{deputy_id}.asp"
+  doc = Nokogiri::HTML(open(fiche))
+  return doc.xpath('//h1[@class="titre"]').first.content
+end
+
+def deputy_seat(deputy_id)
+  fiche = "http://www.assemblee-nationale.fr/13/tribun/fiches_id/#{deputy_id}.asp"
+  doc = Nokogiri::HTML(open(fiche))
+  return doc.xpath('//div[@id="phemi"]/p').first.content.slice(/: (\d+)/, 1)
+end
+
+def parse_sceance(deputies, sceance_url)
+  doc = Nokogiri::HTML(open(sceance_url))
+
+  doc.xpath('//p/b/a[@target="_top"]').each do |link|
+    deputy = link['href'].slice(/(\d+).asp/, 1)
+    
+    if deputies.key?(deputy)
+      deputies[deputy] = deputies[deputy].to_i + 1
+    else
+      deputies[deputy] = 1
+    end
+  end
+end
+
+parse_sceance(deputies, "http://www.assemblee-nationale.fr/13/cri/2008-2009/20090090.asp")
+
+deputies.sort{|a,b| b[1]<=>a[1]}.each { |elem|
+  puts "#{elem[1]}, #{deputy_name(elem[0])} - Place: #{deputy_seat(elem[0])}"
+}
+
+
