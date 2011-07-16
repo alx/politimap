80ff0e897a5f600158a4a5ec85da6edcade56c7b
Alexandre Girard <git@alexgirard.com>
2011-07-16 12:49:17 +0200

    add loading info

--- json_server/public/js/script.js
+++ json_server/public/js/script.js
@@ -4,12 +4,16 @@
 
 var heatmap;
 
-$(document).ready(function(){
-  heatmap = h337.create({"element":document.getElementById("heatmapArea"), "radius":8, "visible":true});
-
+function refreshHeatmap(){
+  $("#heatmapArea").prepend("<span id='heatmap_loading'>chargement...</span>")
   $.getJSON("/heatmap", function(data){
     heatmap.store.setDataSet(data);
+    $("#heatmap_loading").remove();
   });
+}
+$(document).ready(function(){
+  heatmap = h337.create({"element":document.getElementById("heatmapArea"), "radius":8, "visible":true});
+  refreshHeatmap();
 });
 
 
